a6bb0ab0dd419c0bb2d94b221d65583a0bcefcc6
zhanghan1990 <zganghanhan@foxmail.com>
2017-04-26 23:35:04 -0400

    version1

---
+++ constants.py
@@ -0,0 +1,8 @@
+# this is my data ip and port
+IP="127.0.0.1"
+PORT=27017
+# this is data storage path
+basedir="/home/vserver5"
+stockdata=basedir+"/stock data"
+indexdata=basedir+"/index data"
+newdata=basedir+"/newdata"
